5eecf0caf6f6289c06226b692e7ded623854916a
Guido Genzone <61072167+ggenzone@users.noreply.github.com>
2023-12-12 11:56:47 +0100

    feat(bristles/schema): Add basic package to isolate schemas and migrations (#15)

---
+++ packages/schema/src/index.ts
@@ -0,0 +1,9 @@
+export { type FillProperties, FillPropertiesSchema } from './properties/fill'
+export { type StrokeProperties, StrokePropertiesSchema } from './properties/stroke'
+
+export { type DocumentSchemaType, DocumentSchema } from './document'
+export { parseDocument } from './document/parse'
+
+export { type LineSchemaType, LineSchema } from './elements/line'
+export { type RectangleSchemaType, RectangleSchema } from './elements/rectangle'
+export { type TextSchemaType, TextSchema } from './elements/text'
