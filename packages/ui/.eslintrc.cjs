a976d5cd83ff2c269bcc1c6aae745053d8b27b06
Guido Genzone <ggenzone@protonmail.com>
2023-12-12 10:51:10 +0100

    fix(monorepo): Rename repo namespace to @bristles.

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   ignorePatterns: [".eslintrc.cjs"],
-  extends: ["@repo/eslint-config/index.js"],
+  extends: ["@bristles/eslint-config/index.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
